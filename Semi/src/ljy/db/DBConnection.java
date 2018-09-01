798545e3ba3ac80edce791f9a152e41e88f8634d
정연길 <@211.184.248.117>
2018-09-01 11:48:08 +0900

    예인 커밋 11:48

--- Semi/src/ljy/db/DBConnection.java
+++ Semi/src/ljy/db/DBConnection.java
@@ -13,7 +13,7 @@ public class DBConnection {
 		try {
 			Class.forName("oracle.jdbc.OracleDriver");
 			String url="jdbc:oracle:thin:@localhost:1521:xe"; //orcl-> xe
-			conn=DriverManager.getConnection(url, "scott","tiger"); //scott, tiger //yong, admin
+			conn=DriverManager.getConnection(url, "scott","tiger"); ///scott, tiger //yong, admin
 		}catch(ClassNotFoundException ce) {
 			System.out.println(ce.getMessage());
 		}catch(SQLException se) {
